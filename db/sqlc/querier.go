65237ea5648b213a4d8127f53b187bc27cbd1c83
Mike Heft <mikeheft@gmail.com>
2024-09-04 08:50:31 -0600

    Update update query to return row

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -13,7 +13,7 @@ type Querier interface {
 	DeleteAccount(ctx context.Context, id int64) error
 	GetAccount(ctx context.Context, id int64) (Account, error)
 	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
-	UpdateAccount(ctx context.Context, arg UpdateAccountParams) error
+	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
 }
 
 var _ Querier = (*Queries)(nil)
