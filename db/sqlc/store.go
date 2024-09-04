0915440af9995d3101c8ee084007db4c783746ac
Mike Heft <mikeheft@gmail.com>
2024-09-04 12:55:15 -0600

    Start work on transactions

---
+++ db/sqlc/store.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all functions to execture db queries and transactions
+type Store struct {
+	// Composition > Inheritance
+	// The preferred manner in which to extend a struct is to
+	// by embedding
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// extecTx exectures a function within a transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx error: %w, rb err: %w", err, rbErr)
+		}
+	}
+
+	return tx.Commit()
+}
