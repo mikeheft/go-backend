b8ccef83485d581ce37bc34c60be2e53170dc508
Mike Heft <mikeheft@gmail.com>
2024-09-05 15:02:01 -0600

    Add create account endpoint

---
+++ api/server.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/mikeheft/go-backend/db/sqlc"
+)
+
+// Serve all http requests
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to router
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+
+	return server
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
